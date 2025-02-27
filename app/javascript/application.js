b622028e0479b266afa01f217dea496f3be2590c
amar760 <amar.singh@ringcentral.com>
2025-02-27 15:02:28 +0530

    Added social media buttons instead of share button. Icons pending.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "social-share-button"
+import "social-share-button/telegram"
+import "social-share-button/whatsapp"
+import "social-share-button/linkedin"
